54cee20a0872c8f3f698d061c95afa984b81c39e
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-27 18:59:36 +0530

    Setup

---
+++ components/Sidebar/buttons.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Buttons({ title, icon}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Buttons
---
+++ components/Sidebar/sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Buttons from './buttons'
+
+
+function Sidebar() {
+  return (
+    <div className=' h-screen w-1/6 bg-[#091B18]'>
+      <h1>BuildX</h1>
+      
+      <button>
+
+      </button>
+      
+      <div>
+        {/* <Buttons title="" icon={} />
+        <Buttons title="" icon={} />
+        <Buttons title="" icon={} />
+        <Buttons title="" icon={} />
+        <Buttons title="" icon={} />
+        <Buttons title="" icon={} /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
