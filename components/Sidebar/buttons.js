46a7679f7aee199f317a0df1b286e7691da865d3
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-27 19:45:46 +0530

    Added styles!

--- components/Sidebar/buttons.js
+++ components/Sidebar/buttons.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Buttons({ title, icon}) {
   return (
-    <div>
+    <button className="h-12 w-5/6 flex flex-row justify-start items-center hover:bg-[#0e2d27] rounded-lg text-white p-4 sm:flex-row">
+      {icon}
+      <p className="mx-2">{title}</p>
       
-    </div>
+    </button>
   )
 }
 
