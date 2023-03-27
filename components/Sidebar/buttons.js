7e6460a8e75efb1e57a6b80ef6ecdff71d097526
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-27 20:49:09 +0530

    Added responsiveness on sm screens!

--- components/Sidebar/buttons.js
+++ components/Sidebar/buttons.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Buttons({ title, icon}) {
   return (
-    <button className="h-12 w-5/6 flex flex-row justify-start items-center hover:bg-[#0e2d27] rounded-lg text-white p-4 sm:flex-row">
+    <button className="h-13 w-5/6 flex flex-col justify-start 
+    items-center hover:bg-[#0e2d27] rounded-lg text-white 
+    p-4 sm:flex-row sm:h-12"
+    >
       {icon}
       <p className="mx-2">{title}</p>
       
