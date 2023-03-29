b2ec949cc18c975c625a6ff6625991b4a1d3bc8b
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-29 13:43:11 +0530

    Added animation to clipboard icon

--- components/ProjectInfo/InfoButtons.js
+++ components/ProjectInfo/InfoButtons.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function InfoButtons({title, text, icon}) {
     return (
-        <div className="bg-[#0e2d27] flex rounded-xl p-3 m-2 justify-between items-center">
+        <div className="bg-[#0e2d27] flex rounded-xl p-3 m-2 
+            justify-between items-center          
+        ">
             <div className="flex w-1/2 justify-start text-white font-semibold">
                 {title}
             </div>
@@ -10,7 +12,9 @@ function InfoButtons({title, text, icon}) {
                 <div className="text-white text-sm mx-2">
                     {text}
                 </div>
-                <div className="text-white text-sm hover:text-zinc-500">
+                <div className="text-white text-sm hover:text-zinc-500
+                    transition ease-in-out
+                    delay-10 hover:-translate-y-1 duration-100  ">
                     {icon}
                 </div>
 
