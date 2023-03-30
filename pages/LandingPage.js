c03e6f9964db3438c9c6dde3e26143ec962a0c98
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-30 21:33:39 +0530

    Added login state!

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,15 +1,25 @@
 import Navbar from '@/components/LandingPage/Navbar'
 import StartBox from '@/components/LandingPage/StartBox'
 import TextBox from '@/components/LandingPage/TextBox'
-import React from 'react'
+import Login from '@/components/LandingPage/Login'
+import Signup from '@/components/LandingPage/Signup'
+
+
+import React, { useState } from 'react'
+
+
 
 function LandingPage() {
+    const [ login, setLogin ] = useState(true);
+    const handleState = () => {
+        setLogin(!login);
+    }
     return (
         <div className="h-screen w-screen  relative z-0 overflow-hidden">
             <Navbar />
 
             <div className="grid grid-cols-10">
-                <div className="col-span-7 w-screen bg-[#0A1F1C] h-screen px-5 flex flex-col justify-around">
+                <div className="col-span-7 w-screen bg-[#0A1F1C] h-screen px-8 flex flex-col justify-around">
                     <div>
                         <h1 className='text-6xl text-white'>Secure access</h1>
                         <div className="flex mt-5 items-center">
@@ -27,10 +37,17 @@ function LandingPage() {
 
                 </div>
 
-                <div className="col-span-3 flex flex-col h-screen w-full bg-[#102c27]">
-
+                <div className="col-span-3 flex flex-col h-screen w-full bg-[#102c27] justify-center items-center">
+                   
+                    { 
+                        login ? <Login handleState={handleState}/> : <Signup handleState={handleState} />
+                    }
+        
                 </div>
+
             </div>
+
+
 
         </div>
     )
